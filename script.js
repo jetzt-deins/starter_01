652be81cc322be680c04361340a19d7b29ff1085
jetzt-deins.de <75520703+jetzt-deins@users.noreply.github.com>
2022-08-28 08:25:53 +0200

    Icon Change

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
       let mainNav = document.getElementById("js-menu");
       let navBarToggle = document.getElementById("js-navbar-toggle");
+      let icon = document.getElementsByClassName("fa-bars")[0];
 
       navBarToggle.addEventListener("click", function () {
         mainNav.classList.toggle("active");
+        icon.className = icon.className.replace("fa-bars", "fa-plus");
+
       });
