eb33a7051e786e760a48e89c200b17681a64dcad
jetzt-deins.de <75520703+jetzt-deins@users.noreply.github.com>
2022-08-28 08:45:52 +0200

    Update Icon

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-      let mainNav = document.getElementById("js-menu");
-      let navBarToggle = document.getElementById("js-navbar-toggle");
-      let icon = document.getElementsByClassName("fa-bars")[0];
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+let icon = document.getElementsByClassName("fa-bars")[0];
 
-      navBarToggle.addEventListener("click", function () {
-        mainNav.classList.toggle("active");
-        icon.className.replace("fa-bars", "fa-plus");
-      });
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+  icon.classList.toggle("fa-times");
+  
+});
